a104e9f48a76679ea7f1f38132399372d8508989
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-09-03 00:54:31 +0530

    feat: implement async room addition with API integration in room store

--- frontend/src/app/useRoomStore.js
+++ frontend/src/app/useRoomStore.js
@@ -1,16 +1,29 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand";
+import { createRoom } from "../apis/api";
 
 const roomStore = (set) => ({
   roomList: [],
-  addRoom: (room) => {
-    set((state) => ({
-      roomList: [room, ...state.roomList],
-    }));
-  },
-  removeRoom: (roomId) => {
-    set((state) => ({
-        
-    }));
-  },
+  addRoom : async(data) => {
+      try {
+        const res = await createRoom(data)
+        set({
+          roomList : set.roomList.push(res.data)
+        })
+        return res.data
+      } catch (error) {
+        return error.message
+      }
+  }
 });
+
+
+const useRoomStore = create(
+  devtools(
+    persist(roomStore, {
+      name : 'rooms'
+    })
+  )
+)
+
+export default useRoomStore;
